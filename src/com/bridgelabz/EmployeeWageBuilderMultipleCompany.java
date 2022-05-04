63d4b53ad6e8e58b49a145d472a67e470989a3ea
HarishKamaras <harishkamarasu@gmail.com>
2022-05-04 16:52:26 +0530

    RandomAttendance

--- src/com/bridgelabz/EmployeeWageBuilderMultipleCompany.java
+++ src/com/bridgelabz/EmployeeWageBuilderMultipleCompany.java
@@ -3,5 +3,12 @@ package com.bridgelabz;
 public class EmployeeWageBuilderMultipleCompany {
     public static void main(String[] args) {
         System.out.println("Welcome to EmployeeWageBuilderMultipleCompany");
+        int empStatus = (int) (Math.random() * 10) % 2;
+        if(empStatus==1) {
+            System.out.println("Employee present");
+        }
+        else{
+            System.out.println("Employee Absent");
+        }
     }
 }
